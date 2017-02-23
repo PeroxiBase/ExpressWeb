8bf84cf96126505973afc39f3fb4bddffee3c669
Bruno SAVELLI <savelli@lrsv.ups-tlse.fr>
2017-02-23 14:10:02 +0100

    V1.1

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'session','user_agent','ion_auth');
+$autoload['libraries'] = array('database','session', 'user_agent','ion_auth');
 
 /*
 | -------------------------------------------------------------------
